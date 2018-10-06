c060a1a5069b9a0d21439174ce12f0578b65b074
mithhu <mithhu666@gmail.com>
2018-10-06 18:57:50 +0600

    Updates

---
+++ precache-manifest.f181c7241119a400f51ff1836d29daec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9a40debb74d3aff6ba",
+    "url": "/lyricsfinder/static/js/runtime~main.3f9a40de.js"
+  },
+  {
+    "revision": "97b0faf73a9295d941eb",
+    "url": "/lyricsfinder/static/js/main.97b0faf7.chunk.js"
+  },
+  {
+    "revision": "6a9a6594b10bed7e788a",
+    "url": "/lyricsfinder/static/js/1.6a9a6594.chunk.js"
+  },
+  {
+    "revision": "97b0faf73a9295d941eb",
+    "url": "/lyricsfinder/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "ad78182bb63dad2387c007c5717b5d6d",
+    "url": "/lyricsfinder/index.html"
+  }
+];
